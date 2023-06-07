ac1b5286bcda5b863fa749c526dcedc6f3be81fe
Andrei <andreizakharov@protonmail.com>
2023-06-07 20:13:48 +0300

    Include images, sprites, fonts // Better naming and comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,128 @@
+//
+// Working files: work/*.html
+//                work/scss/style.scss
+//                work/js/main.js
+//                work/images/src
+//
+// To initialize gulp: `npm i`
+// To start working: `gulp`
+// To build the project: `gulp build`
+//
+
 const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify-es").default;
+// Styles and scripts
+const scss = require("gulp-sass")(require("sass")); // css
+const autoprefixer = require("gulp-autoprefixer"); // for older browser versions
+const uglify = require("gulp-uglify-es").default; // js
+const concat = require("gulp-concat"); // concatination
+
+// Images and sprites
+const avif = require("gulp-avif");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer"); // don't repeat tasks
+const svgSprite = require("gulp-svg-sprite");
+
+// Fonts
+const fonter = require("gulp-fonter");
+const ttf2woff2 = require("gulp-ttf2woff2");
+
+// Watch and build
 const browserSync = require("browser-sync").create();
-const autoprefixer = require("gulp-autoprefixer");
 const clean = require("gulp-clean");
 
+//
+// Included functions: styles, scripts, images, sprite, fonts, watching,
+// cleanBuild, fillBuild
+//
+
 function styles() {
-  return src("app/scss/style.scss")
+  return src("work/scss/style.scss")
     .pipe(autoprefixer({ overrideBrowserlist: ["last 10 version"] }))
     .pipe(concat("style.min.css"))
     .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("app/css"))
+    .pipe(dest("work/css"))
     .pipe(browserSync.stream());
 }
 
 function scripts() {
-  return src("app/js/main.js")
+  return src("work/js/main.js")
     .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest("app/js"))
+    .pipe(dest("work/js"))
     .pipe(browserSync.stream());
 }
 
+function images() {
+  return src(["work/images/src/*.*", "!work/images/src/*.svg"])
+    .pipe(newer("work/images"))
+    .pipe(avif({ quality: 50 })) // convert to avif
+    .pipe(src("work/images/src/*.*"))
+    .pipe(newer("work/images"))
+    .pipe(webp()) // convert to webp
+    .pipe(src("work/images/src/*.*"))
+    .pipe(newer("work/images"))
+    .pipe(imagemin()) // just minify
+    .pipe(dest("work/images"));
+}
+
+function sprite() {
+  return src("work/images/src/*.svg")
+    .pipe(
+      svgSprite({ mode: { stack: { sprite: "../sprite.svg", example: true } } })
+    ) // unify all the SVGs in one sprite file
+    .pipe(dest("work/images"));
+}
+
+function fonts() {
+  return src("work/fonts/src/*.*")
+    .pipe(fonter({ formats: ["woff", "ttf"] }))
+    .pipe(src("work/fonts/*.ttf"))
+    .pipe(ttf2woff2())
+    .pipe(dest("work/fonts"));
+}
+
 function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+  browserSync.init({ server: { baseDir: "work/" } });
+  watch(["work/scss/style.scss"], styles);
+  watch(["work/js/main.js"], scripts);
+  watch(["work/images/src"], images, sprite);
+  watch(["work/fonts/src"], fonts);
+  watch(["work/**/*.html"]).on("change", browserSync.reload);
 }
 
-function browsersync() {
-  browserSync.init({ server: { baseDir: "app/" } });
+function cleanBuild() {
+  return src("build").pipe(clean());
 }
 
-function cleanPublic() {
-  return src("public").pipe(clean());
+function fillBuild() {
+  return src(
+    [
+      "work/css/style.min.css",
+      "work/js/main.min.js",
+      "work/*.html",
+      "work/images/*.*",
+      "!work/images/*.svg",
+      "work/images/sprite.svg",
+      "work/fonts/*.*",
+    ],
+    {
+      base: "work",
+    }
+  ).pipe(dest("build"));
 }
 
-function fillPublic() {
-  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
-    base: "app",
-  }).pipe(dest("public"));
-}
+//
+// Main commands: `gulp`, `gulp build`
+//
 
 exports.styles = styles;
 exports.scripts = scripts;
+exports.images = images;
+exports.sprite = sprite;
+exports.fonts = fonts;
 exports.watching = watching;
-exports.browsersync = browsersync;
 
-exports.build = series(cleanPublic, fillPublic);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, watching);
+exports.build = series(cleanBuild, fillBuild);
